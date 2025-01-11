abc49a67dc2ed1b4ea37c7b63fb766d4db0568c1
Arthur Loureiro <arthur.loureiro@fysik.su.se>
2025-01-11 15:48:51 +0100

    added function to extract systematic parameters from yaml

--- CosmoAPI/two_pt_func/generate_theory.py
+++ CosmoAPI/two_pt_func/generate_theory.py
@@ -1,8 +1,11 @@
 import numpy as np
+from typing import Dict, Tuple, List, Any
+
 import firecrown
 from firecrown.metadata_functions import make_all_photoz_bin_combinations
 import firecrown.likelihood.two_point as tp
 from firecrown.utils import base_model_from_yaml
+from firecrown.updatable import UpdatableCollection
 
 from CosmoAPI.two_pt_func.nz_loader import load_all_redshift_distr
 from CosmoAPI.two_pt_func.tracer_tools import process_probes_load_2pt
@@ -10,8 +13,8 @@ from CosmoAPI.two_pt_func.tracer_tools import generate_ell_theta_array
 from CosmoAPI.not_implemented import not_implemented_message
 from CosmoAPI.firecrown_tools import load_systematics_factory
 
-
-def generate_two_point_metadata(yaml_data, two_point_function, two_pt_probes, 
+#FIXME: Add type hints
+def _generate_two_point_metadata(yaml_data: dict, two_point_function, two_pt_probes, 
                                 two_point_bins):
     """
     Generate the metadata for the two-point functions based on the YAML data.
@@ -37,7 +40,7 @@ def generate_two_point_metadata(yaml_data, two_point_function, two_pt_probes,
         xtype = 'ell_bins'
         ells_list = []
         for p in two_pt_probes:
-            ells_list.append(generate_ell_theta_array_from_yaml(yaml_data['probes'][p], xtype))
+            ells_list.append(generate_ell_theta_array(yaml_data['probes'][p], xtype))
         all_two_point_metadata = [two_point_function(XY=ij, ells=ell) 
                                  for ij, ell in zip(two_point_bins, ells_list)]
     elif two_point_function is firecrown.metadata_types.TwoPointReal:
@@ -54,7 +57,21 @@ def generate_two_point_metadata(yaml_data, two_point_function, two_pt_probes,
         raise ValueError("Unknown TwoPointFunction type")
     return all_two_point_metadata
 
-def prepare_2pt_functions(yaml_data):
+def prepare_2pt_functions(yaml_data: dict) -> Tuple[UpdatableCollection, List[Any]]:
+    """
+    Prepare the two-point functions based on the YAML data.
+
+    Parameters
+    ----------
+    yaml_data : dict
+        Parsed YAML data in dictionary format.
+    
+    Returns
+    -------
+    tuple
+        A tuple containing the two-point functions and the metadata 
+        for the two-point functions.
+    """
     # here we call this X because we do not know if it is ell_bins or theta_bins
     two_point_function, two_pt_probes = process_probes_load_2pt(yaml_data)
 
@@ -80,7 +97,7 @@ def prepare_2pt_functions(yaml_data):
             raise ValueError(f"Unknown systematics type: {type_factory} for probe {p}")
 
     # generate the metadata for the two-point functions
-    all_two_point_metadata = generate_two_point_metadata(yaml_data,
+    all_two_point_metadata = _generate_two_point_metadata(yaml_data,
                                                          two_point_function,
                                                          two_pt_probes.keys(),
                                                          all_two_point_bins)
